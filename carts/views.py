c2489d65d336beab3e3a95ce90d10abc3f6a8c77
‘JabedAhmedRifat’ <jabedahmedrifat1@gmail.com>
2022-11-15 22:30:53 +0600

    starting product variation

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render ,redirect, get_object_or_404
-from store.models import Product
+from store.models import Product , Variation
 from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -16,6 +16,20 @@ def _cart_id(request):
 
 
 def add_cart(request , product_id):
+    product = Product.objects.get(id=product_id) # get the product_id
+    product_variation =[]
+    if request.method == 'POST':
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+
+            try:
+                variation = Variation.objects.get(product = product, variation_category__iexact=key, variation_value__iexact=value)
+                product_variation.append(variation)
+            except:
+                pass
+
+
     product = Product.objects.get(id=product_id) # get the product_id
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request)) # get the cart using cart_id present in the session
