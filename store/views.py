af0ae81586507991a0c52a5aa60cf4917c599ecd
‘JabedAhmedRifat’ <jabedahmedrifat1@gmail.com>
2022-11-15 09:51:22 +0600

    added searching functionality

--- store/views.py
+++ store/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
 from carts.models import CartItem
+from django.db.models import Q
 
 from carts.views import _cart_id
 from django.core.paginator import EmptyPage, PageNotAnInteger , Paginator
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -49,3 +51,18 @@ def product_detail(request, category_slug, product_slug):
     }
 
     return render (request , 'store/product_detail.html' , context)
+
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+
+    context={
+        'products' : products,
+        'product_count' : products.count,
+    }
+    return render(request, 'store/store.html' ,context)
