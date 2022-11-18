340ee0e7a36e57f551498d94c282ecb05764382b
‘JabedAhmedRifat’ <jabedahmedrifat1@gmail.com>
2022-11-18 09:50:51 +0600

    just add dashboard template and some button

--- accounts/views.py
+++ accounts/views.py
@@ -69,8 +69,8 @@ def login(request):
 
         if user is not None:
             auth.login(request , user)
-            # messages.success(request , "You are now logged in.")
-            return redirect('home')
+            messages.success(request , "You are now logged in.")
+            return redirect('dashboard')
         else:
             messages.error(request, 'Invalid login credentials')
             return redirect('login')
@@ -105,3 +105,9 @@ def activate(request , uidb64 ,token):
     else:
         messages.error(request,'Invalid activation link')
         return redirect('register')
+
+
+
+@login_required(login_url = 'login')
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
