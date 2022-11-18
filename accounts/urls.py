6130e5b58ceeb0f9fc0a9f0778df8d524e0af09b
‘JabedAhmedRifat’ <jabedahmedrifat1@gmail.com>
2022-11-18 23:30:13 +0600

    forget password function added

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
 
     path('activate/<uidb64>/<token>/', views.activate , name='activate'),
 
+    path('forgotPassword/', views.forgotPassword , name='forgotPassword'),
+    path('resetpassword_validate/<uidb64>/<token>/', views.resetpassword_validate , name='resetpassword_validate'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -111,3 +111,39 @@ def activate(request , uidb64 ,token):
 @login_required(login_url = 'login')
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
+
+
+
+
+def forgotPassword(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        if Account.objects.filter(email=email).exists():
+            user = Account.objects.get(email__iexact=email)
+
+            # Reset password email
+
+            current_site = get_current_site(request)
+            mail_subject = 'Reset Your Password'
+            message = render_to_string('accounts/reset_password_email.html',{
+                'user': user,
+                'domain': current_site,
+                'uid' : urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject , message, to=[to_email])
+            send_email.send()
+
+            messages.success(request, 'Password reset email has been sent to your email address.')
+            return redirect('login')
+
+        else:
+            messages.error(request, 'Account does not exist')
+            return redirect('forgotPassword')
+    return render(request, 'accounts/forgotPassword.html')
+
+
+
+def resetpassword_validate(request):
+    return HttpResponse('ok')
