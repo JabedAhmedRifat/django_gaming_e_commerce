7b566c034c7aba1c7527b63ddb793123bc9451d1
‘JabedAhmedRifat’ <jabedahmedrifat1@gmail.com>
2022-11-24 23:51:41 +0600

    secure added

--- gamesquest/settings.py
+++ gamesquest/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'li43ek)j8r__jsj1tgi^2lnzs(^w*rji^99paye0(*6(^q$kbh'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -42,6 +44,7 @@ INSTALLED_APPS = [
     'store',
     'carts',
     'orders',
+    'admin_honeypot',
 ]
 
 MIDDLEWARE = [
@@ -52,8 +55,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_session_timeout.middleware.SessionTimeoutMiddleware',
 ]
 
+SESSION_EXPIRE_SECONDS = 120
+SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
+SESSION_TIMEOUT_REDIRECT = 'accounts/login'
+                                                                                                                                                              
 ROOT_URLCONF = 'gamesquest.urls'
 
 TEMPLATES = [
@@ -145,8 +153,8 @@ MESSAGE_TAGS = {
 
 
 # SMTP CONFIGURATION
-EMAIL_HOST ='smtp.gmail.com'
-EMAIL_PORT = 587 #DIFFERENT HOST WILL VARIES THE PORT NUMBER
-EMAIL_HOST_USER  = 'jabedahmedrifat2@gmail.com'
-EMAIL_HOST_PASSWORD = 'tuhwcmnlnhmnmnjz'
-EMAIL_USE_TLS = True
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER  = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
