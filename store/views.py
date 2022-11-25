c4bc5eb3a6170caaf3d2266a3059365daaae7c06
‘JabedAhmedRifat’ <jabedahmedrifat1@gmail.com>
2022-11-25 20:33:19 +0600

    added store's page rating

--- store/views.py
+++ store/views.py
@@ -21,13 +21,13 @@ def store(request , category_slug=None):
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available= True)
-        paginator = Paginator(products,3)
+        paginator = Paginator(products,4)
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True).order_by('id')
-        paginator = Paginator(products,3)
+        paginator = Paginator(products,6)
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
         product_count = products.count()
